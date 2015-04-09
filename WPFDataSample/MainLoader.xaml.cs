aa848c19cbf96fa7b2709bfd7770101d39d793fd
ArpoJul <julio.polo.arias@gmail.com>
2015-04-09 14:39:07 -0500

    Modificación completa de la aplicación: Se implementara el manejo de Empresas completa con la estructura de SIGAC

---
+++ WPFDataSample/MainLoader.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDataSample
+{
+    /// <summary>
+    /// Interaction logic for MainLoader.xaml
+    /// </summary>
+    public partial class MainLoader : Window
+    {
+        public MainLoader()
+        {
+            InitializeComponent();
+        }
+
+        private void PaisesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Paises();
+            newWindow.Show();
+        }
+    }
+}
---
+++ WPFDataSample/Paises.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDataSample
+{
+    /// <summary>
+    /// Interaction logic for Paises.xaml
+    /// </summary>
+    public partial class Paises : Window
+    {
+        public Paises()
+        {
+            InitializeComponent();
+        }
+
+        private void Paises1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource util_PaisesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("util_PaisesViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // util_PaisesViewSource.Source = [generic data source]
+        }
+    }
+}
